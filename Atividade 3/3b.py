b6e70ce8e8e3d192aa547abf0c94921278c53478
Kleber Vasconcelos <klebervasconcelos@MacBook-Pro-de-Kleber.local>
2022-11-22 14:18:50 -0300

    update

--- Atividade 3/3b.py
+++ Atividade 3/3b.py
@@ -6,11 +6,10 @@ def solveFunction(expression, a, b, N):
 
     x, y = symbols('x y')
     res = sympify(expression)
-    res = res.subs(x, a)
-    # plot(sympify(expression), (x, a, b), line_color='red')
-    plot(4, (x, a, b))
+    # res = res.subs(N)
+    plot(sympify(expression), (x, a, b), line_color='red')
 
     return res
 
 
-solveFunction("2*x+5", -5, 5, [1, 2, 3, 4, 5])
+print(solveFunction("2*x+5", -5, 5, 3))
